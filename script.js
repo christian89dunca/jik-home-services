597ac5b85500c212a1b7bb72586aedb017eff8be
Christian Dunca <christiandunca@mac.mynetworksettings.com>
2024-12-31 19:51:21 -0700

    wip

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+const currnetMoment = new Date();
+
 const outlineLogo = document.getElementById("outline-logo");
 const originalLogo = document.getElementById('original-logo')
 const nav = document.getElementsByTagName('nav')[0];
+const copyrightYear = document.getElementById('copyright-year');
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    copyrightYear.innerText = currnetMoment.getFullYear();
+})
 
 window.addEventListener('scroll', function() {
     if (window.scrollY > 0){
