da5f254ae345877b9623cebc3204b06e5ea00117
Christian Dunca <christiandunca@mac.mynetworksettings.com>
2025-01-02 22:16:23 -0700

    wip

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     const currnetMoment = new Date();
     const body = document.getElementsByTagName('body')[0];
+    const logoContainer = document.getElementById('logo-container');
     const outlineLogo = document.getElementById("outline-logo");
     const originalLogo = document.getElementById('original-logo')
     const nav = document.getElementsByTagName('nav')[0];
@@ -16,7 +17,6 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     copyrightYear.innerText = currnetMoment.getFullYear();
 
-    renderMenuContent('exterior');
 
     function triggerMenu(){
         if(closedBurger.classList.contains('visible')){
@@ -91,7 +91,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
                     menuTabs[0].classList.add('current-step');
                     tabsContent[0].classList.add('show');
                     menuTabs.forEach(tab => {
-                        tab.addEventListener('click', ()=>{
+                        tab.addEventListener('mouseenter', ()=>{
                             menuTabs.forEach(tab => tab.classList.remove('current-step'));
                             tabsContent.forEach(element => element.classList.remove('show'));
                             tab.classList.add('current-step');
@@ -100,45 +100,14 @@ document.addEventListener('DOMContentLoaded', ()=>{
                         })
                     })
 
-                    // tabsContent.forEach(tab => tab.classList.remove('show'))
-
         } catch (error) {
             console.error(error)
         }
-        
-
-    
-        // <div id="tab1" class="menu-tab current-step">
-        //             <span>1</span>
-        //             <p>Cover the area</p>
-        //         </div>
-        //         <div id="tab2" class="menu-tab">
-        //             <span>2</span>
-        //             <p>Prep the Walls</p>
-        //         </div>
-        //         <div id="tab3" class="menu-tab">
-        //             <span>3</span>
-        //             <p>Apply new paint</p>
-        //         </div>
-        //         <div id="tab4" class="menu-tab">
-        //             <span>4</span>
-        //             <p>Clean-Up</p>
-        //         </div>
-        //         <div id="tab1-content" class="tabcontent">
-        //             We don’t want dust or paint getting where it doesn’t need to be. Our house painters’ first step is to lay drop cloths and plastic over the floors and furniture in the space we are painting.
-        //         </div>
-        //         <!-- <div id="tab1-content" class="tabcontent">
-        //             Our expert house painters will next apply your quality paint. The time of this step depends on the surface and the paint, but our painters will make sure to work quickly and efficiently.
-        //         </div> -->
-        //         <!-- <div id="tab1-content" class="tabcontent">
-        //             Since a smooth wall surface makes for better paint adhesion, our house painters will then prep the surfaces before painting.
-        //         </div> -->
-        //         <!-- <div id="tab1-content" class="tabcontent">
-        //             We want to make sure your house looks like new and that includes our house painters cleaning up after our paint job.
-        //         </div> -->
     }
 
-    
+    logoContainer.addEventListener('click', ()=>{
+        window.location.href = 'index.html'
+    })
 
     burgerMenu.addEventListener('click', () => {
         triggerMenu();
@@ -146,14 +115,17 @@ document.addEventListener('DOMContentLoaded', ()=>{
             triggerNavbarStyle(openedMenu.classList.contains('visible'));
         }
     })
-
+    // renderMenuContent('exterior');
     const menuLiItems = Array.from(menuItems).map(element => element.parentElement);
+    renderMenuContent(menuLiItems[0].firstChild.innerText.replace(menuLiItems[0].firstChild.innerText[0], menuLiItems[0].firstChild.innerText[0].toLowerCase()).split(" ").join(''));
     menuLiItems.forEach(element => element.addEventListener('mouseenter', (event)=>{
         // console.log(element.firstChild.innerText.replace(element.firstChild.innerText[0], element.firstChild.innerText[0].toLowerCase()).split(" ").join(''));
         menuLiItems.forEach(element => element.classList.remove('selected'))
         element.classList.add('selected');
-        renderMenuContent(element.firstChild.innerText.replace(element.firstChild.innerText[0], element.firstChild.innerText[0].toLowerCase()).split(" ").join(''))
+        renderMenuContent(element.firstChild.innerText.replace(element.firstChild.innerText[0], element.firstChild.innerText[0].toLowerCase()).split(" ").join(''));
     }))
+
+    
     
 
 
